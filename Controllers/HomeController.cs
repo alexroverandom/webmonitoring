12ced3d046ba63791013b240da100efc36ba6f1c
Sergey Alekseenko <S.Alekseenko@tscrm.com>
2018-10-12 09:21:33 +0300

    add test telegram bot

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,15 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Telegram.Bot;
+using Telegram.Bot.Types;
 
 namespace WebMonitoring.Controllers
 {
 	public class HomeController : Controller
 	{
+		private const string Token = "636110092:AAGH0rsJTdfeFrg7mAzWnHfJmIOFrwxbLAA";
+		private const long ChatId = -283586034;
+
 		public IActionResult Index()
 		{
 			return View();
 		}
 		public IActionResult Test() {
 			return View();
+		}
+
+		public async Task<string> Notify(int id) {
+			var client = new TelegramBotClient(Token);
+			var me = client.GetMeAsync().Result;
+			var message = await client.SendTextMessageAsync(new ChatId(ChatId), $"You sent {id}");
+			return "Ok";
 		}
 	}
 }
